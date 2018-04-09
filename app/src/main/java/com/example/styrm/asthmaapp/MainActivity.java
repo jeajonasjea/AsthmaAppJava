43f94d97d5d21d931a6ab8206e46055fc8b1f350
casper <styrmandsmolf@gmail.com>
2018-04-09 12:17:12 +0200

    lavet spørgeskema activity færdigt

--- app/src/main/java/com/example/styrm/asthmaapp/MainActivity.java
+++ app/src/main/java/com/example/styrm/asthmaapp/MainActivity.java
@@ -32,6 +32,5 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(i2);
             }
         });
-
     }
 }
--- app/src/main/java/com/example/styrm/asthmaapp/Spoergeskema.java
+++ app/src/main/java/com/example/styrm/asthmaapp/Spoergeskema.java
@@ -1,7 +1,14 @@
 package com.example.styrm.asthmaapp;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+
+import java.io.FileOutputStream;
 
 public class Spoergeskema extends AppCompatActivity {
 
@@ -9,5 +16,45 @@ public class Spoergeskema extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spoergeskema);
+
+        Button button;
+
+        button = (Button) findViewById(R.id.svarKnap);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String svarGodt = "";
+                String svarDårligt = "";
+                CheckBox boxGodt = findViewById(R.id.godtBox);
+                CheckBox boxDårligt = findViewById(R.id.dårligBox);
+                String svar;
+
+                if(boxGodt.isSelected() == true){
+                    svarGodt = "true";
+                    svarDårligt = "false";
+                }
+                else if(boxDårligt.isSelected() == true){
+                    svarGodt = "false";
+                    svarDårligt = "true";
+                }
+
+                 svar = svarGodt + "," + svarDårligt + ",";
+
+                String filename = "myFile";
+                FileOutputStream outputstream;
+
+                try{
+                    outputstream = openFileOutput(filename, Context.MODE_PRIVATE);
+                    outputstream.write(svar.getBytes());
+
+                }catch (Exception e) {
+                e.printStackTrace();
+                }
+                Intent returnToMain = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(returnToMain);
+            }
+        });
+
     }
+
 }
